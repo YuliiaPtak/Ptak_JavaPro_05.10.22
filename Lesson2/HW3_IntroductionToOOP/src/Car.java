582fee9136fd154c3ff15e02e7d7df012daad16e
YuliiaPtak <yui01122015@gmail.com>
2022-10-17 14:04:24 +0300

    Finished hw3

---
+++ Lesson2/HW3_IntroductionToOOP/src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+    }
+
+    private void startCommand() {
+    }
+
+    private void startFuelSystem() {
+    }
+}
