7473560276783cd81b71ae082f2c7f8a954b80a0
YuliiaPtak <yui01122015@gmail.com>
2022-11-10 13:00:07 +0200

    Created my own linked list

--- ArrayList/src/LinkedListNew.java
+++ ArrayList/src/LinkedListNew.java
@@ -1,23 +1,19 @@
-public class LinkedListNew implements MyList {
-    private Node first = new Node();
-    private Node last = new Node();
-    private int size;
+import java.util.Arrays;
 
-    public LinkedListNew() {
-        first.next = last;
-        last.prev = first;
-    }
+public class LinkedListNew implements MyList {
+    private Node first;
+    private Node last;
+    private int size;
 
     @Override
     public boolean add(Object o) {
-        Node node = new Node();
-        node.value = o;
-
-        Node tempNode = last.prev;
-        tempNode.next = node;
-        node.prev = tempNode;
-        last.prev = node;
-
+        Node lastNode = last;
+        Node newNode = new Node(lastNode, o, null);
+        last = newNode;
+        if (lastNode == null)
+            first = newNode;
+        else
+            lastNode.next = newNode;
         size++;
 
         return true;
@@ -25,20 +21,58 @@ public class LinkedListNew implements MyList {
 
     @Override
     public boolean remove(Object o) {
-        Node currentElement = first.next;
-        while (currentElement != null) {
-            if (currentElement.value.equals(o)) {
-                //todo unlink
-                return true;
+        if (o == null) {
+            for (Node current = first; current != null; current = current.next) {
+                if (current.value == null) {
+                    unlink(current);
+                    return true;
+                }
             }
-            currentElement = currentElement.next;
+        } else {
+            for (Node current = first; current != null; current = current.next) {
+                if (o.equals(current.value)) {
+                    unlink(current);
+                    return true;
+                }
+            }
         }
+
         return false;
+    }
+
+    private void unlink(Node node) {
+        Node next = node.next;
+        Node prev = node.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            node.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            node.next = null;
+        }
+
+        node.value = null;
+        size--;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] objects = new Object[size];
+        Node current = first;
+
+        for (int i = 0; current != null; i++) {
+            objects[i] = current.value;
+            current = current.next;
+        }
+
+        return objects;
     }
 
     @Override
@@ -53,25 +87,46 @@ public class LinkedListNew implements MyList {
 
     @Override
     public void clear() {
+        for (Node current = first; current != null; ) {
+            Node next = current.next;
+            current.value = null;
+            current.next = null;
+            current.prev = null;
+            current = next;
+        }
 
+        first = last = null;
+        size = 0;
     }
 
     @Override
     public Object get(int index) {
-        return null;
-    }
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds for size " + size);
+        }
 
-    public void print() {
-        Node currentElement = first.next;
-        while ((currentElement) != null) {
-            System.out.println(currentElement.value);
+        Node currentElement = first;
+        for (int i = 0; i < index; i++) {
             currentElement = currentElement.next;
         }
+
+        return currentElement.value;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(toArray());
     }
 
     public static class Node {
         private Node prev;
         private Object value;
         private Node next;
+
+        public Node(Node prev, Object value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
--- ArrayList/src/Test.java
+++ ArrayList/src/Test.java
@@ -1,25 +1,36 @@
-import java.util.LinkedList;
-
 public class Test {
     public static void main(String[] args) {
-//        ArrayListNew arrayListNew = new ArrayListNew();
-//
-//        for (int i = 0; i < 10; i++) {
-//            arrayListNew.add(i);
-//        }
-//
-//        arrayListNew.remove(5);
-//        System.out.println(arrayListNew + "\n" + arrayListNew.size() + "\n" + arrayListNew.get(5) + "\n" + arrayListNew.isEmpty());
-//
-//        arrayListNew.clear();
-//        System.out.println(arrayListNew.isEmpty());
+        ArrayListNew arrayListNew = new ArrayListNew();
+
+        for (int i = 0; i < 10; i++) {
+            arrayListNew.add(i);
+        }
+
+        System.out.println("ArrayList:");
+        System.out.println(arrayListNew + "\nsize = " + arrayListNew.size());
+        System.out.println("Remove 5");
+        arrayListNew.remove(5);
+        System.out.println(arrayListNew + "\nsize = " + arrayListNew.size() + "\narrayList[3] = " + arrayListNew.get(3));
+        System.out.println("isEmpty = " + arrayListNew.isEmpty());
+        System.out.println("Clear:");
+        arrayListNew.clear();
+        System.out.println("isEmpty = " + arrayListNew.isEmpty() + "\nsize = " + arrayListNew.size());
+
+        System.out.println("\nLinkedList:");
 
         LinkedListNew linkedListNew = new LinkedListNew();
-        linkedListNew.add(5);
-        linkedListNew.add(3);
-        linkedListNew.add(9);
-        linkedListNew.print();
-        linkedListNew.remove(9);
-        linkedListNew.print();
+
+        for (int i = 0; i < 10; i++) {
+            linkedListNew.add(i);
+        }
+
+        System.out.println(linkedListNew + "\nsize = " + linkedListNew.size());
+        System.out.println("Remove 5");
+        linkedListNew.remove(5);
+        System.out.println(linkedListNew + "\nsize = " + linkedListNew.size() + "\narrayList[3] = " + linkedListNew.get(3));
+        System.out.println("isEmpty = " + linkedListNew.isEmpty());
+        System.out.println("Clear:");
+        linkedListNew.clear();
+        System.out.println("isEmpty = " + linkedListNew.isEmpty() + "\nsize = " + linkedListNew.size());
     }
 }
