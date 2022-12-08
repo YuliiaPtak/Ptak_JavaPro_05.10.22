abc3d314e06bef7288ae6ac5fc192fde7d42db77
YuliiaPtak <yui01122015@gmail.com>
2022-12-08 15:15:09 +0200

    Fixed hw15

--- HW15_MapAndSet/src/Main.java
+++ HW15_MapAndSet/src/Main.java
@@ -1,18 +1,17 @@
 import coffee.order.CoffeeOrderBoard;
-import coffee.order.exceptions.OrderAmountException;
 
 public class Main {
     public static void main(String[] args) {
         CoffeeOrderBoard COFFEE_ORDER_BOARD = new CoffeeOrderBoard();
-            COFFEE_ORDER_BOARD.add("Bob");
-            COFFEE_ORDER_BOARD.add("Jack");
-            COFFEE_ORDER_BOARD.add("Rick");
-            COFFEE_ORDER_BOARD.add("Tom");
+        COFFEE_ORDER_BOARD.add("Bob");
+        COFFEE_ORDER_BOARD.add("Jack");
+        COFFEE_ORDER_BOARD.add("Rick");
+        COFFEE_ORDER_BOARD.add("Tom");
 
-            COFFEE_ORDER_BOARD.draw();
-            COFFEE_ORDER_BOARD.deliver();
-            COFFEE_ORDER_BOARD.draw();
-            COFFEE_ORDER_BOARD.deliver(COFFEE_ORDER_BOARD.getNumber("Rick"));
-            COFFEE_ORDER_BOARD.draw();
+        COFFEE_ORDER_BOARD.draw();
+        COFFEE_ORDER_BOARD.deliver();
+        COFFEE_ORDER_BOARD.draw();
+        COFFEE_ORDER_BOARD.deliver(COFFEE_ORDER_BOARD.getNumber("Rick"));
+        COFFEE_ORDER_BOARD.draw();
     }
 }
