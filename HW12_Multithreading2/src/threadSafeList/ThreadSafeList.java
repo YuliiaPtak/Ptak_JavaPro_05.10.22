1770271aa9916a6eaf528bcb38a81c261a613cdb
YuliiaPtak <yui01122015@gmail.com>
2022-12-09 21:20:02 +0200

    corrected ThreadSafeList

--- HW12_Multithreading2/src/threadSafeList/ThreadSafeList.java
+++ HW12_Multithreading2/src/threadSafeList/ThreadSafeList.java
@@ -1,22 +1,24 @@
 package threadSafeList;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ThreadSafeList<E> {
     private final List<E> threadSafeList = new ArrayList<>();
 
-    public synchronized boolean add(E value) {
-        return threadSafeList.add(value);
+    public boolean add(E value) {
+        synchronized (threadSafeList) {
+            return threadSafeList.add(value);
+        }
     }
 
-    public synchronized E remove(int index) {
-        return threadSafeList.remove(index);
+    public E remove(int index) {
+        synchronized (threadSafeList) {
+            return threadSafeList.remove(index);
+        }
     }
 
-    public synchronized E get(int index) {
+    public E get(int index) {
         return threadSafeList.get(index);
     }
 }
